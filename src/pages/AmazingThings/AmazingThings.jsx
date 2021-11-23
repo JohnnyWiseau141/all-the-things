f40f0a6cb4e5f44981fcb496c4aea29c8a011ec3
WangManLex <alexandermaniwang99@gmail.com>
2021-11-23 11:44:02 -0500

    stub stuff

---
+++ src/pages/AmazingThings/AmazingThings.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ThingCard from "../../components/ThingCard/ThingCard";
+import { Link } from "react-router-dom";
+
+const AmazingThings = (props) => {
+  return ( 
+    <>
+      <Link to="/"> Home </Link>
+
+
+    </>
+  );
+}
+ 
+export default AmazingThings;
