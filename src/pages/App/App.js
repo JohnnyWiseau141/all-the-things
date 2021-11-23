3f717a17a9b1b2c85c4ff86b6214ce8cbfd63a6c
WangManLex <alexandermaniwang99@gmail.com>
2021-11-23 11:24:01 -0500

    add alex's stuff

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -20,12 +20,26 @@ const [gracesThings] = useState([
   },
 ])
 
+const [alexsThings] = useState([
+  {
+    name: "my mom",
+    image: "https://www.telegraph.co.uk/content/dam/beauty/2016/06/22/Grace-Timothy-xlarge_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpg",  
+    attributes: ["yes", "yes", "not a taco", "amazing"],
+  },
+  {
+    name: "Muscle Man",
+    image: "https://wallpapercave.com/wp/wp3600371.jpg",
+    attributes: ["Screaming", "Green", "not a taco", "is mom"], 
+  },
+])
+
   return (
     <>
       <Routes>
         <Route path='/' element={<Home/>} />
         {/* All the <Route> components should live here */}
         <Route path='/the-functional-things' element={<FunctionalThings things={gracesThings} />} />
+        <Route path='/the-amazing-things' element={<FunctionalThings things={alexsThings} />} />
       </Routes>
     </>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ const Home = (props) => {
       {/* All the <a> tags should live here */}
       <h1>All-The-Things</h1>
       <Link to="/the-functional-things">Grace's Things</Link><br/>
+      <Link to="/the-amazing-things">Alex's Things</Link><br/>
     </>
   );
 }
